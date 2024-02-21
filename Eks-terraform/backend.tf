b4711af3fedc101d64c2cd3901394de8ce1d1840
Sai Ketha <99094794+Saiketha@users.noreply.github.com>
2024-02-21 09:57:51 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "saikethasbucketmb" # Replace with your actual S3 bucket name
+    bucket = "tonystarkbucketmb" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
