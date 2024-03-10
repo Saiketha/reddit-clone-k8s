a29da86c5033879773c4315edab62ce1647533a5
Sai Ketha <99094794+Saiketha@users.noreply.github.com>
2024-03-10 14:37:54 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "redditsaibucket" # Replace with your actual S3 bucket name
+    bucket = "saibucketreddit" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
